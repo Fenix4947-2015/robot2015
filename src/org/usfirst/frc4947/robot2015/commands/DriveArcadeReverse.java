53d25a2e2cfb420ac31b220a37ca36bdb77d6c4b
Steeves Bouchard <steeves.bouchard@ge.com>
2015-02-07 19:04:17 -0500

    Ajout d'une commande pour avancer en ligne droite Ajout d'une commande pour conduire à l'envers avec la 2ieme manette de XBox

---
+++ src/org/usfirst/frc4947/robot2015/commands/DriveArcadeReverse.java
@@ -0,0 +1,48 @@
+package org.usfirst.frc4947.robot2015.commands;
+
+import org.usfirst.frc4947.robot2015.OI.XBoxAxis;
+import org.usfirst.frc4947.robot2015.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveArcadeReverse extends Command {
+
+    public DriveArcadeReverse() {
+        requires(Robot.driveTrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+		// TODO Voir si on a besoin d'une protection au niveau de la vitesse maximale
+    	Robot.driveTrain.setMaxOutput(0.5);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	double moveValue = Robot.oi.getJoystickHelperAxis(XBoxAxis.RightTrigger) - Robot.oi.getJoystickHelperAxis(XBoxAxis.LeftTrigger);
+    	double rotateValue = Robot.oi.getJoystickHelperAxis(XBoxAxis.LeftStickX, 0.1);
+    	
+    	Robot.driveTrain.arcadeDrive(-moveValue, rotateValue);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+    	// Execute this command until interrupted
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	// Stop the robot when interrupted
+    	Robot.driveTrain.tankdrive(0, 0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
---
+++ src/org/usfirst/frc4947/robot2015/commands/DriveStraight.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc4947.robot2015.commands;
+
+import org.usfirst.frc4947.robot2015.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DriveStraight extends Command {
+
+	private double timeout;
+	
+    public DriveStraight(double timeout) {
+        requires(Robot.driveTrain);
+        
+        this.timeout = timeout;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	setTimeout(timeout);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.driveTrain.tankdrive(0.5, 0.5);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return isTimedOut();
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    	// Stop the robot when interrupted
+    	Robot.driveTrain.tankdrive(0, 0);
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	end();
+    }
+}
